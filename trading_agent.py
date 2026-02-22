6ce00f80a04504055462706b8acb895fe9f2c183
bouwebas-droid <bouwebas@gmail.com>
2026-02-22 21:24:09 +0100

    Update trading_agent.py

--- trading_agent.py
+++ trading_agent.py
@@ -3,18 +3,15 @@ from coinbase_agentkit import AgentKit, AgentKitConfig
 from coinbase_agentkit.wallet_providers.local_evm_wallet_provider import LocalEvmWalletProvider
 
 def setup_synthora():
-    # Haal private key + RPC uit environment variables
     private_key = os.environ["EVM_PRIVATE_KEY"]
     rpc_url = os.environ["EVM_RPC_URL"]
 
-    # Lokale wallet provider (GEEN Coinbase meer)
     wallet = LocalEvmWalletProvider(
         private_key=private_key,
         rpc_url=rpc_url,
-        chain_id=8453  # Base mainnet
+        chain_id=8453
     )
 
-    # AgentKit met lokale wallet
     agent_kit = AgentKit(
         AgentKitConfig(
             network_id="base-mainnet",
@@ -22,5 +19,4 @@ def setup_synthora():
         )
     )
 
-    # Bouw de agent zoals Synthora dat verwacht
     return build_agent_executor(agent_kit)
